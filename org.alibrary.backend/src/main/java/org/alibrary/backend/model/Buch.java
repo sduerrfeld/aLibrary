09ff3c961e5d9852108b39df29eeef6ff1a34f22
whiskerz <whiskerz@cougar.(none)>
2011-03-24 19:37:12 +0100

    trying another reset in the hope of finding a version with working eclipse settings

--- org.alibrary.backend/src/main/java/org/alibrary/backend/model/Buch.java
+++ org.alibrary.backend/src/main/java/org/alibrary/backend/model/Buch.java
@@ -1,27 +1,49 @@
 package org.alibrary.backend.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
-import org.alibrary.backend.model.Ausleihstatus;
-import org.alibrary.backend.model.Markierung;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.hibernate.annotations.Type;
 import org.hibernate.validator.constraints.NotEmpty;
 import org.joda.time.LocalDate;
 
+@Entity
 public class Buch {
+
+	@Id
+	@GeneratedValue
+	private Long id;
 	
-	@NotNull
-	@Pattern(regexp = "ISBN\\x20(?=.{13}$)\\d{1,5}([- ])\\d{1,7}\\1\\d{1,6}\\1(\\d|X)$")
+	@NotNull(message="isbn may not be null")
+	@Pattern(regexp = "ISBN\\x20(?=.{13}$)\\d{1,5}([- ])\\d{1,7}\\1\\d{1,6}\\1(\\d|X)$", message="isbn not valid")
 	private String isbn;
-	@NotEmpty
+	
+	@NotEmpty(message="titel may not be null")
 	private String titel;
 	private String autor;
-	@NotNull
-	private Markierung markierung;
+	
+	@NotEmpty(message="markierung may not be empty")
+	private String markierung;
+	
 	private String ausleiher;
+	
+	@Type(type="org.joda.time.contrib.hibernate.PersistentLocalDate")
 	private LocalDate ausleihdatum;
-	@NotNull
-	private Ausleihstatus ausleihstatus;
+	
+	@NotEmpty(message="ausleihstatus may not be empty")
+	private String ausleihstatus;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getISBN() {
 		return isbn;
@@ -47,11 +69,11 @@ public class Buch {
 		this.autor = author;
 	}
 
-	public void setMarkierung(final Markierung markierung) {
+	public void setMarkierung(final String markierung) {
 		this.markierung = markierung;
 	}
 
-	public Markierung getMarkierung() {
+	public String getMarkierung() {
 		return markierung;
 	}
 
@@ -71,12 +93,27 @@ public class Buch {
 		this.ausleihdatum = ausleihdatum;
 	}
 
-	public Ausleihstatus getAusleihstatus() {
+	public String getAusleihstatus() {
 		return ausleihstatus;
 	}
 
-	public void setAusleihstatus(final Ausleihstatus status) {
+	public void setAusleihstatus(final String status) {
 		this.ausleihstatus = status;
 	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof Buch)) {
+			return false;
+		}
+		Buch buch = (Buch) obj;
+		
+		return this.id == buch.id;
+	}
+	
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder().append(id).toHashCode();
+	}
+	
 }
