be9e44ea63dc899d2e6eea6c0ca0d8f9d4d0605a
stefan.duerrfeld <stefan.duerrfeld@andrena.de>
2011-06-29 19:38:54 +0200

    Test fuer das Speichern von Buechern erweitert

--- org.alibrary.backend/src/test/java/org/alibrary/backend/dao/BuchDaoTest.java
+++ org.alibrary.backend/src/test/java/org/alibrary/backend/dao/BuchDaoTest.java
@@ -14,22 +14,31 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration(locations={"classpath:applicationContext-backend.xml"})
 public class BuchDaoTest {
 
+	private static final String MARKIERUNG = "Roter Punkt";
+	private static final String AUSLEIHSTATUS = "Nicht ausgeliehen";
+	private static final String TITEL = "Ein Buch";
+	private static final String ISBN = "ISBN 0 93028 923 4";
+	
 	@Resource
 	private BuchDao buchDao;
 
 	@Test
 	public void testSpeichereBuch() {
 		Buch buch = new Buch();
-		buch.setISBN("ISBN 0 93028 923 4");
-		buch.setTitel("Ein Buch");
-		buch.setAusleihstatus("Nicht ausgeliehen");
-		buch.setMarkierung("Roter Punkt");
+		buch.setISBN(ISBN);
+		buch.setTitel(TITEL);
+		buch.setAusleihstatus(AUSLEIHSTATUS);
+		buch.setMarkierung(MARKIERUNG);
 		
 		Buch gespeichertesBuch = buchDao.speichereBuch(buch);
 		
 		Buch buchAusDatenbank = buchDao.ladeBuch(gespeichertesBuch.getId());
 		
 		assertEquals(gespeichertesBuch, buchAusDatenbank);
+		assertEquals(ISBN, buchAusDatenbank.getISBN());
+		assertEquals(TITEL, buchAusDatenbank.getTitel());
+		assertEquals(AUSLEIHSTATUS, buchAusDatenbank.getAusleihstatus());		
+		assertEquals(MARKIERUNG, buchAusDatenbank.getMarkierung());		
 	}
 	
 }
